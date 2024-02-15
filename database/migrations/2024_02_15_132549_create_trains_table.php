c86bc74d85700f5649e44a982324e557c0da3057
AimanFadil <aimanbello2001@gmail.com>
2024-02-15 14:45:25 +0100

    created table with columns

--- database/migrations/2024_02_15_132549_create_trains_table.php
+++ database/migrations/2024_02_15_132549_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('agency', 40);
+            $table->string('departure_station', 30);
+            $table->string('arrival_station', 30);
+            $table->dateTime('departure_time');
+            $table->dateTime('arrival_time');
+            $table->bigInteger('id_train')->unsigned();
+            $table->tinyInteger('carriage_number')->unsigned();
+            $table->boolean('is_puntual');
+            $table->boolean('is_available');
             $table->timestamps();
         });
     }
